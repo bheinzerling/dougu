4b8f266be82f2f3cb578947fbf77d7a8a0ce5b3d
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2017-08-07 18:02:36 +0200

    Add some regexes

--- dougu/strings.py
+++ dougu/strings.py
@@ -1,5 +1,23 @@
+import re
+
+
+re_digit = re.compile("\d")
+re_http_url = re.compile(r"https?:\/\/[^ ]*")
+re_www_url = re.compile(r"www.[^ ]*")
+
+
 def capitalize(s):
     try:
         return s[0].upper() + s[1:].lower()
     except IndexError:
         return s[0].upper()
+
+
+def normalize_digits(text):
+    return re_digit.sub("0", text)
+
+
+def normalize_urls(text):
+    text = re_http_url.sub("<url>", text)
+    text = re_www_url.sub("<url>", text)
+    return text
