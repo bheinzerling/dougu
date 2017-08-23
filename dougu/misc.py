c65d4bbdd104b6679988a7fe593bd8d2d3797b6c
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2017-08-23 13:54:39 +0200

    move results

--- dougu/misc.py
+++ dougu/misc.py
@@ -31,10 +31,13 @@ class Results():
         self.fname_pkl = Path(str(file) + ".df.pkl")
         self.fname_txt = Path(str(file) + ".txt")
         self.fname_stats = Path(str(file) + ".overall_stats")
+        self.results = df()
         if self.fname_pkl.exists():
-            self.results = joblib.load(self.fname_pkl)
-        else:
-            self.results = df()
+            try:
+                self.results = joblib.load(self.fname_pkl)
+            except EOFError:
+                import traceback
+                traceback.print_exc()
 
     def append(self, results_row):
         row = df([results_row])
