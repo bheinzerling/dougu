1926950761ffb9b0a4df36a1599fd7ef38b048d8
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2018-01-24 12:12:12 +0100

    Re-add LengthBatcher

--- dougu/torchutil.py
+++ dougu/torchutil.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from collections import defaultdict
+from pprint import pprint
 
 import numpy as np
 import torch
 from torch import nn
-
-from .io import load_word2vec_file
+from torch.cuda import LongTensor
 
 
 # fix inconsistencies between cuda and non-cuda tensors when
@@ -26,30 +26,60 @@ if torch.cuda.is_available():
 else:
     from torch import Tensor, LongTensor  # NOQA
 
+# if torch.cuda.is_available():
+#     from torch.cuda import FloatTensor, LongTensor
+#     Tensor = FloatTensor
+# else:
+#     from torch import Tensor, LongTensor  # NOQA
+#     FloatTensor = Tensor
+
 
 class LengthBatcher():
     def __init__(
-            self, X, Y=None, batch_size=100, get_len=lambda x: x[1] - x[0]):
+            self, X, Y=None, batch_size=100,
+            get_len=lambda x: x[1] - x[0], keys=None,
+            start_ends=False):
         self.X = X
         self.Y = Y
         self.batch_size = batch_size
-        len2idxs = defaultdict(list)
-        for idx in range(len(X)):
-            len2idxs[get_len(X[idx])].append(idx)
-        self.len2idxs = {l: LongTensor(idxs) for l, idxs in len2idxs.items()}
-        self.lengths = np.array(list(self.len2idxs.keys()))
+        if start_ends:
+            keys = X[:, 1] - X[:, 0]
+        if keys is None:
+            len2idxs = defaultdict(list)
+            for idx in range(len(X)):
+                len2idxs[get_len(X[idx])].append(idx)
+            self.len2idxs = {l: LongTensor(idxs) for l, idxs in len2idxs.items()}
+            self.lengths = np.array(list(self.len2idxs.keys()))
+            self.multilen = self.lengths.ndim > 1
+        else:
+            self.lengths = list(set(keys.cpu().tolist()))
+            self.len2idxs = {
+                l: torch.nonzero(keys == l).squeeze()
+                for l in self.lengths}
+            self.multilen = False
 
     def __iter__(self):
         if torch.cuda.is_available():
             if self.Y is not None:
-                yield from self.iter_XY()
+                if self.multilen:
+                    raise NotImplementedError
+                else:
+                    yield from self.iter_XY()
             else:
-                yield from self.iter_X()
+                if self.multilen:
+                    yield from self.iter_X_multi()
+                else:
+                    yield from self.iter_X()
         else:
             if self.Y is not None:
                 yield from self.iter_XY_cpu()
             else:
                 yield from self.iter_X_cpu()
+
+    def __len__(self):
+        return sum(
+            len(idxs.split(self.batch_size))
+            for idxs in self.len2idxs.values())
 
     def iter_XY(self):
         np.random.shuffle(self.lengths)
@@ -63,6 +93,14 @@ class LengthBatcher():
         np.random.shuffle(self.lengths)
         for length in self.lengths:
             idxs = self.len2idxs[length]
+            shuf_idxs = torch.randperm(idxs.shape[0]).cuda()
+            for batch_idxs in idxs[shuf_idxs].split(self.batch_size):
+                yield self.X[batch_idxs]
+
+    def iter_X_multi(self):
+        np.random.shuffle(self.lengths)
+        for lengths in self.lengths:
+            idxs = self.len2idxs[tuple(lengths)]
             shuf_idxs = torch.randperm(idxs.shape[0]).cuda()
             for batch_idxs in idxs[shuf_idxs].split(self.batch_size):
                 yield self.X[batch_idxs]
