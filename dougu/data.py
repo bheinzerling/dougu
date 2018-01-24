6306bdbeea380d2c450b0ab0ff2ca4f89b4aa7f2
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2018-01-24 12:14:45 +0100

    Add ensure_iob2

--- dougu/data.py
+++ dougu/data.py
@@ -230,3 +230,25 @@ def print_cm(
                 cell = cell if cm[i, j] > hide_threshold else empty_cell
             print(cell, end=" ")
         print()
+
+
+# https://github.com/glample/tagger/blob/master/utils.py
+def ensure_iob2(tags):
+    """Check that tags have a valid IOB format.
+    Tags in IOB1 format are converted to IOB2.
+    """
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            continue
+        split = tag.split('-')
+        if len(split) != 2 or split[0] not in ['I', 'B']:
+            return False
+        if split[0] == 'B':
+            continue
+        elif i == 0 or tags[i - 1] == 'O':  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+        elif tags[i - 1][1:] == tag[1:]:
+            continue
+        else:  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+    return True
