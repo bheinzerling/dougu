cdc0c065a18b9208139ad1705b282137d159b52d
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2019-10-30 18:40:43 +0900

    torchutil.py

--- dougu/torchutil.py
+++ dougu/torchutil.py
@@ -714,6 +714,7 @@ class RandomSplits(Splits):
                 for split_length in self.split_lengths]
         else:
             splits = random_split(instances, self.split_lengths)
+        breakpoint()
         return splits
 
 
