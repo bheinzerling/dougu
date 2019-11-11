05cbc7e484a4522a6c595f67ccb6a3d31ff81895
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2019-11-11 12:06:14 +0900

    transformers.py

--- dougu/transformers.py
+++ dougu/transformers.py
@@ -41,10 +41,10 @@ class Transformer():
         self.model = AutoModel.from_pretrained(model_name)
         device_count = torch.cuda.device_count()
         print('device count:', device_count)
-        breakpoint()
         if device_count > 1:
             self.model = torch.nn.DataParallel(self.model)
         self.model.to(device=self.device)
+        breakpoint()
         self.max_len = max_len or self.tokenizer.max_len
         self.dim = self.model.embeddings.position_embeddings.weight.size(1)
         if self.model_name.startswith('roberta'):
