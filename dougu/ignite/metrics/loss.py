eb37710ef7715a79dbb74c2479e4499dc3375421
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2019-11-18 14:19:19 +0900

    ignite/metrics/loss.py

--- dougu/ignite/metrics/loss.py
+++ dougu/ignite/metrics/loss.py
@@ -41,7 +41,6 @@ class Loss(Metric):
             kwargs = {}
         else:
             y_pred, y, kwargs = output
-        breakpoint()
         average_loss = self._loss_fn(y_pred, y, **kwargs)
 
         if len(average_loss.shape) != 0:
