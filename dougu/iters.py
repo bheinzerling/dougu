8ba72a8a4cfea32d1e088db56572937cf95465d5
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2019-12-18 12:34:04 +0900

    iters.py

--- dougu/iters.py
+++ dougu/iters.py
@@ -169,7 +169,7 @@ class LazyList(Sequence):
     @cached_property
     def items(self):
         items = list(self.generator)
-        self.log(f'loaded {len(items)} {self.name})
+        self.log(f'loaded {len(items)} {self.name}')
 
     def __getitem__(self, idx):
         return self.items[idx]
