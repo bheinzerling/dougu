8829da9bb33fe01fa9af7de505aac6b3fb2ed9d9
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2020-01-15 14:54:25 +0900

    ignite/metrics/mean_reciprocal_rank.py

---
+++ dougu/ignite/metrics/mean_reciprocal_rank.py
@@ -0,0 +1,25 @@
+import torch
+
+from .metric import Metric
+
+
+class MeanReciprocalRank(Metric):
+
+    def reset(self, mode='prob'):
+        self.ranks = []
+        self.update = {
+            'prob': self.update_prob,
+            'idx': self.update_idx}[mode]
+
+    def update_prob(self, output):
+        probs, target = output
+        if (probs < 0).any():
+            assert (probs <= 0).all()
+            probs = torch.exp(probs)
+        correct_prob = probs.gather(1, target.unsqueeze(1))
+        rank = (probs >= correct_prob).sum(dim=1)
+        self.ranks.append(rank.cpu())
+
+    def compute(self):
+        rank = torch.cat(self.ranks)
+        return (1 / rank.float()).mean()
