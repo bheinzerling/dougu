c10b61d056a3f1a9e6ec55a918bc92bbce352049
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2020-01-27 17:52:08 +0900

    transformers.py

--- dougu/transformers.py
+++ dougu/transformers.py
@@ -22,6 +22,7 @@ class Transformer():
     def __init__(self, model_name, device=None, max_len=None):
         super().__init__()
         breakpoint()
+        self.randinit = model_name.endswith('-randinit'):
         self.model_name = model_name
         self.device = device or _device
         self.log = get_logger()
