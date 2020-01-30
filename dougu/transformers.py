32ef5cf7b4cd2df195f19341f580b033f363b8a4
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2020-01-30 15:51:04 +0900

    transformers.py

--- dougu/transformers.py
+++ dougu/transformers.py
@@ -66,7 +66,8 @@ class Transformer():
         self.model.to(device=self.device)
         self.max_len = max_len or self.tokenizer.max_len
         word_emb = self.model.get_input_embeddings().weight
-        self.vocab_size, self.dim = word_emb.size()
+        self.vocab_size = word_emb.size(0)
+        self.dim = word_emb.size(1)
         self.pad_idx = self.tokenizer.pad_token_id
 
     def update_special_tokens(self, additional_special_tokens):
