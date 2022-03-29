99c2e53e64e5d1ddb4daf1d32d9339da74ecf12b
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2022-03-29 17:40:22 +0900

    add popularity

---
+++ dougu/wikidata/popularity.py
@@ -0,0 +1,35 @@
+import torch
+
+from dougu import (
+    cached_property,
+    )
+
+from .wikidata_attribute import WikidataAttribute
+
+
+class WikidataPopularity(WikidataAttribute):
+    @cached_property
+    def raw(self):
+        raise NotImplementedError('todo')
+
+    @cached_property
+    def tensor(self):
+
+        def counts(node_ids):
+            node_ids = torch.sort(node_ids).values
+            node_ids, counts = torch.unique_consecutive(
+                node_ids, return_counts=True)
+            all_counts = torch.zeros(
+                self.wikidata.n_entities, dtype=counts.dtype)
+            all_counts[node_ids] = counts
+            return all_counts
+
+        indegree = counts(self.wikidata.relations.tensor[:, 2])
+        outdegree = counts(self.wikidata.relations.tensor[:, 0])
+
+        return {
+            'indegree': indegree,
+            'outdegree': outdegree,
+            'sum_in_out': indegree + outdegree,
+            }
+
