080f7eaf65a2159eb03efbcdd2de5c73d5fdae7a
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2022-05-30 02:33:52 +0900

    handle tokenizers without bos

--- dougu/transformer_mixin.py
+++ dougu/transformer_mixin.py
@@ -17,6 +17,13 @@ class WithTransformerEncoder(Configurable):
         ]
     _max_seq_len = None
 
+    @property
+    def conf_fields(self):
+        return super().conf_fields + [
+            'max_seq_len',
+            'transformer',
+            ]
+
     @cached_property
     def tokenizer(self):
         from transformers import AutoTokenizer
@@ -36,7 +43,18 @@ class WithTransformerEncoder(Configurable):
     def max_seq_len(self):
         return self._max_seq_len or self.conf.max_seq_len
 
-    def encode_texts(self, texts, output_hidden_states=True):
+    @property
+    def bos_offset(self):
+        # use ._bos_token instead of .bos_token to avoid warning
+        return 0 if self.tokenizer._bos_token is None else 1
+
+    def encode_texts(
+            self,
+            texts,
+            output_hidden_states=True,
+            output_fp16=False,
+            output_device='cpu',
+            ):
         from tqdm import tqdm
         from boltons.iterutils import chunked
 
@@ -56,7 +74,7 @@ class WithTransformerEncoder(Configurable):
                 )
             subw_lens = tok_out['attention_mask'].sum(dim=1)
             word_start_mask = torch.zeros_like(tok_out['input_ids'])
-            word_start_mask[:, 1] = 1
+            word_start_mask[:, self.bos_offset] = 1
             word_ids = torch.zeros_like(tok_out['input_ids']) - 100
 
             word_start_idxs = torch.zeros_like(tok_out['input_ids'])
@@ -65,10 +83,10 @@ class WithTransformerEncoder(Configurable):
             for i, text in enumerate(chunk):
                 subw_len = subw_lens[i]
                 word_id = torch.tensor(
-                    tok_out.word_ids(i)[1:subw_len - 1])
-                word_ids[i, 1:subw_len - 1] = word_id
+                    tok_out.word_ids(i)[self.bos_offset:subw_len - 1])
+                word_ids[i, self.bos_offset:subw_len - 1] = word_id
                 diff = torch.diff(word_id)
-                word_start_mask[i, 2:subw_len - 1] = diff
+                word_start_mask[i, self.bos_offset + 1:subw_len - 1] = diff
                 n_words = len(text)
                 for word_idx in range(n_words):
                     token_span = tok_out.word_to_tokens(i, word_idx)
@@ -87,6 +105,7 @@ class WithTransformerEncoder(Configurable):
 
             with torch.no_grad():
                 self.trf.eval()
+                self.prepare_model_inputs(tok_out)
                 trf_out = self.trf(
                     **tok_out.to(self.conf.trf_enc_device),
                     output_hidden_states=output_hidden_states,
@@ -103,24 +122,32 @@ class WithTransformerEncoder(Configurable):
                     except KeyError:
                         pass
             chunk_tensors.update(dict(trf_out))
+            if output_fp16:
+                chunk_tensors = {k: v.half() for k, v in chunk_tensors.items()}
 
             for k, v in chunk_tensors.items():
                 if isinstance(v, torch.Tensor):
+                    v = v.to(device=output_device)
                     tensors[k].append(v)
 
-        return {k: torch.cat(v).cpu() for k, v in tensors.items()}
+        return {k: torch.cat(v) for k, v in tensors.items()}
+
+    def prepare_model_inputs(self, tok_out):
+        pass
 
 
 class WithTransformerLM(WithTransformerEncoder):
     @cached_property
     def trf(self):
         from transformers import (
+            AutoModelWithLMHead,
             AutoModelForMaskedLM,
             AutoModelForSeq2SeqLM,
             AutoModelForCausalLM,
             )
         exception = None
         for automodel_cls in [
+                AutoModelWithLMHead,
                 AutoModelForMaskedLM,
                 AutoModelForSeq2SeqLM,
                 AutoModelForCausalLM,
@@ -129,7 +156,15 @@ class WithTransformerLM(WithTransformerEncoder):
                 return automodel_cls.from_pretrained(
                     self.conf.transformer,
                     ).to(device=self.conf.trf_enc_device)
-            except ValueError() as e:
+            except ValueError as e:
                 exception = e
         else:
             raise exception
+
+    def prepare_model_inputs(self, tok_out):
+        gen_inputs = self.trf.prepare_inputs_for_generation(**tok_out)
+        if 'attention_mask' in gen_inputs:
+            assert (gen_inputs['attention_mask'] == tok_out.attention_mask).all()
+        for k, v in gen_inputs.items():
+            if v is not None:
+                tok_out.data[k] = v
