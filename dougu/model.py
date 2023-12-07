1d1eeb7e17ef3d394635b89a3223ec2fe84ef744
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2023-12-08 08:24:44 +0900

    add log

--- dougu/model.py
+++ dougu/model.py
@@ -3,9 +3,10 @@ from torch import nn
 from dougu import (
     Configurable,
     SubclassRegistry,
+    WithLog,
     )
 
 
-class ModelBase(Configurable, SubclassRegistry, nn.Module):
+class ModelBase(Configurable, SubclassRegistry, WithLog, nn.Module):
     def __init__(self, conf):
         super().__init__(conf)
