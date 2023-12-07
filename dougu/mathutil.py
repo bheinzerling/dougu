09ea36974a2f23c9e8d45c5f2771322ec58ee2fc
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2023-12-08 08:22:56 +0900

    add decimal_round

--- dougu/mathutil.py
+++ dougu/mathutil.py
@@ -3,3 +3,11 @@ def avg(values):
     if n_values:
         return sum(values) / n_values
     return 0
+
+
+def decimal_round(number, n_decimal_places=0):
+    from decimal import Decimal, ROUND_HALF_UP
+    round_fmt = '1'
+    if n_decimal_places > 0:
+        round_fmt += '.' + '0' * n_decimal_places
+    return Decimal(number).quantize(Decimal(round_fmt), rounding=ROUND_HALF_UP)
